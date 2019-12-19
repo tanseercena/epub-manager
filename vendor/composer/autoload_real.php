9f413bf8c40fc93fade1def8888508aed7b98d6c
asfiaaiman <aiman.asfia@gmail.com>
2019-12-19 15:40:34 +0500

    book import from excel implemented

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit6518143538b8c361746bfbe10ba0c5a2
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit6518143538b8c361746bfbe10ba0c5a2::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire6518143538b8c361746bfbe10ba0c5a2($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire6518143538b8c361746bfbe10ba0c5a2($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
