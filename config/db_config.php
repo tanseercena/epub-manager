ea77145ea66d15173b5cc21aa4efcbc0f90ac542
Suleman <suleman@viralwebbs.com>
2019-12-19 12:59:02 +0500

    implemented logout functionality

--- config/db_config.php
+++ config/db_config.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 // $db_host = "localhost";
 // $db_username = "root";
 // $db_password = "";
 // $db_name = "epub_track";
 
 return [
-    'db_host' => 'localhost',
+    'db_host' => 'localhost:8889',
     'db_username' => 'root',
-    'db_password' => '',
+    'db_password' => 'root',
     'db_name' => 'epub_track'
 ];
