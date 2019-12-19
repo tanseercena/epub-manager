b0a62b02247edc3e8b63f92f3a884c0495ed5e6b
Tanseer UL Hassan <tanseercena@gmail.com>
2019-12-19 14:24:56 +0500

    Update db_config.php
    Update db_config.php file.

--- config/db_config.php
+++ config/db_config.php
@@ -1,12 +1,8 @@
-<?php
-// $db_host = "localhost";
-// $db_username = "root";
-// $db_password = "";
-// $db_name = "epub_track";
+<?php 
 
 return [
-    'db_host' => 'localhost:8889',
+    'db_host' => 'localhost',
     'db_username' => 'root',
-    'db_password' => 'root',
+    'db_password' => '',
     'db_name' => 'epub_track'
 ];
