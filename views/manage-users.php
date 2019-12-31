17847759cf5d5da88d119ff4346d1f9f3f33b766
tanseercena <tanseercena@gmail.com>
2019-12-31 12:50:47 +0500

    Fix Manage User

--- views/manage-users.php
+++ views/manage-users.php
@@ -7,11 +7,7 @@ require_once __DIR__ . "/layouts/header.php";
 <div class="content-wrapper">
     <div class="content">
         <div class="breadcrumb-wrapper breadcrumb-contacts">
-<<<<<<< HEAD
-            <h1>Departments</h1>
-=======
             <h1>Users</h1>
->>>>>>> 784d43ebfac253ce399c59a87aed900d41097aa4
 
             <!-- <nav aria-label="breadcrumb">
                 <ol class="breadcrumb p-0">
@@ -27,43 +23,11 @@ require_once __DIR__ . "/layouts/header.php";
                 </ol>
             </nav> -->
             <div>
-<<<<<<< HEAD
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-add-contact"> Add New Department
-=======
                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-add-user"> Add New User
->>>>>>> 784d43ebfac253ce399c59a87aed900d41097aa4
                 </button>
             </div>
         </div>
 
-<<<<<<< HEAD
-        
-        <!-- Add Department model Button -->
-        <div class="modal fade" id="modal-add-contact" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-                <div class="modal-content">
-                    <form action="../actions/add_department.php" method="post">
-                        <div class="modal-header px-4">
-                            <h5 class="modal-title" id="exampleModalCenterTitle">Create New Department</h5>
-                        </div>
-                        <div class="modal-body px-4">
-
-                            <div class="form-group row mb-6">
-
-                                <div class="col-sm-8 col-lg-10">
-                                    <div class="form-group">
-                                        <label for="formGroupExampleInput">Add Department</label>
-                                        <input type="text" name="name" required class="form-control" id="formGroupExampleInput" placeholder="Add New Department">
-                                    </div>
-                                </div>
-                            </div>
-
-
-                        </div>
-                        <div class="modal-footer px-4">
-                            <button type="button" class="btn btn-secondary btn-pill" data-dismiss="modal">Cancel</button>
-                            <button type="submit" class="btn btn-primary btn-pill">Save</button>
-=======
         <!-- Add User Button  -->
         <div class="modal fade" id="modal-add-user" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -127,7 +91,6 @@ require_once __DIR__ . "/layouts/header.php";
                         <div class="modal-footer px-4">
                             <button type="button" class="btn btn-secondary btn-pill" data-dismiss="modal">Cancel</button>
                             <button type="submit" class="btn btn-primary btn-pill">Save Contact</button>
->>>>>>> 784d43ebfac253ce399c59a87aed900d41097aa4
                         </div>
                     </form>
                 </div>
@@ -139,11 +102,7 @@ require_once __DIR__ . "/layouts/header.php";
                 <!-- Recent Order Table -->
                 <div class="card card-table-border-none" id="recent-orders">
                     <div class="card-header justify-content-between">
-<<<<<<< HEAD
-                        <h2>All Departments</h2>
-=======
                         <h2>All Users</h2>
->>>>>>> 784d43ebfac253ce399c59a87aed900d41097aa4
                         <div class="
                          ">
                             <span></span>
@@ -153,35 +112,18 @@ require_once __DIR__ . "/layouts/header.php";
                         <table class="table card-table table-responsive table-responsive-large" style="width:100%">
                             <thead>
                                 <tr>
-<<<<<<< HEAD
-                                    <th>Department ID</th>
-                                    <th>Department Name</th>
-=======
                                     <th>User ID</th>
                                     <th>User Name</th>
                                     <th>Email</th>
                                     <th>Department</th>
                                     <th>Password</th>
                                     <th>Status</th>
->>>>>>> 784d43ebfac253ce399c59a87aed900d41097aa4
                                     <th></th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php
 
-<<<<<<< HEAD
-                                $query = new Department();
-                                $departments = $query->all();
-                                foreach ($departments as $department) {
-                                ?>
-                                    <tr>
-                                        <td><?php echo $department['id']; ?></td>
-                                        <td>
-                                            <a class="text-dark" href=""> <?php echo $department['name']; ?></a>
-                                        </td>
-
-=======
                                 $query = new User();
                                 $users = $query->all();
                                 // print_r($users);
@@ -214,19 +156,12 @@ require_once __DIR__ . "/layouts/header.php";
                                         <td>
                                             <p class="text-dark"> <?php echo $user['status'] == 1 ? "Active" : "Inactive";  ?></p>
                                         </td>
->>>>>>> 784d43ebfac253ce399c59a87aed900d41097aa4
 
                                         <td class="text-right">
                                             <div class="dropdown show d-inline-block widget-dropdown">
                                                 <a class="dropdown-toggle icon-burger-mini" href="" role="button" id="dropdown-recent-order1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-display="static"></a>
                                                 <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown-recent-order1">
                                                     <li class="dropdown-item">
-<<<<<<< HEAD
-                                                        <a href="#" onclick="showEditForm(<?php echo $department['id']; ?>,'<?php echo $department['name']; ?>')">Edit</a>
-                                                    </li>
-                                                    <li class="dropdown-item">
-                                                        <a href="../actions/delete_department.php?department_id=<?php echo $department['id'] ?>">Delete</a>
-=======
                                                         <a href="#" onclick="showEditForm(<?php echo $user['id']; ?>,'<?php echo $user['name']; ?>')">Edit</a>
                                                     </li>
                                                     <li class="dropdown-item">
@@ -240,7 +175,6 @@ require_once __DIR__ . "/layouts/header.php";
                                                             echo  "<a href='../actions/user_status.php?user_id= " . $user['id'] . "&status=1'>Unblock</a>";
                                                         }
                                                         ?>
->>>>>>> 784d43ebfac253ce399c59a87aed900d41097aa4
                                                     </li>
                                                 </ul>
                                             </div>
@@ -335,23 +269,14 @@ require_once __DIR__ . "/layouts/header.php";
 
 </div>
 
-<<<<<<< HEAD
-=======
 <!-- Edit User Name -->
->>>>>>> 784d43ebfac253ce399c59a87aed900d41097aa4
 <div class="modal fade" id="modal-add-edit" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <form action="../actions/update_department.php" method="post">
-<<<<<<< HEAD
-                <input type="hidden" name="department_id" id="department_id">
-                <div class="modal-header px-4">
-                    <h5 class="modal-title" id="exampleModalCenterTitle">Edit Department Name</h5>
-=======
                 <input type="hidden" name="user_id" id="user_id">
                 <div class="modal-header px-4">
                     <h5 class="modal-title" id="exampleModalCenterTitle">Edit User Name</h5>
->>>>>>> 784d43ebfac253ce399c59a87aed900d41097aa4
                 </div>
                 <div class="modal-body px-4">
 
@@ -359,13 +284,8 @@ require_once __DIR__ . "/layouts/header.php";
 
                         <div class="col-sm-8 col-lg-10">
                             <div class="form-group">
-<<<<<<< HEAD
-                                <label for="formGroupExampleInput">Edit Department Name</label>
-                                <input type="text" name="name" id="dep_name" required class="form-control" id="formGroupExampleInput" placeholder="Add New Department">
-=======
                                 <label for="formGroupExampleInput">Edit User Name</label>
                                 <input type="text" name="name" id="user_name" required class="form-control" id="formGroupExampleInput" placeholder="Add New User">
->>>>>>> 784d43ebfac253ce399c59a87aed900d41097aa4
                             </div>
                         </div>
                     </div>
@@ -382,15 +302,9 @@ require_once __DIR__ . "/layouts/header.php";
 </div>
 
 <script>
-<<<<<<< HEAD
-    function showEditForm(dep_id, dep_name) {
-        $("#department_id").val(dep_id);
-        $("#dep_name").val(dep_name);
-=======
     function showEditForm(user_id, user_name) {
         $("#user_id").val(user_id);
         $("#user_name").val(user_name);
->>>>>>> 784d43ebfac253ce399c59a87aed900d41097aa4
         $('#modal-add-edit').modal('show');
     }
 </script>
