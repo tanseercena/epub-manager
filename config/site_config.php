bd4560a4a79a126cbef258ef5b33d2d1d133161e
faizadev <saeednumra@gmail.com>
2020-01-01 12:27:18 +0500

    Fix

--- config/site_config.php
+++ config/site_config.php
@@ -2,7 +2,4 @@
 $site_name = "Epub Track";
 $logo= "assets/images/logo.png";
 $default_email = "info@amdb.com";
-<<<<<<< HEAD
-=======
 $base_path = "/epub-manager";
->>>>>>> c230c496b8506d57f0e4c8761e5e272644a99031
