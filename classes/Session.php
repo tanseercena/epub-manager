1814959c3a42becc16f74e8dbb15d9e1db912156
humaismail-95 <humaismail19@gmail.com>
2020-01-01 11:36:25 +0600

    your message

---
+++ classes/Session.php
@@ -0,0 +1,53 @@
+<?php
+
+class Session{
+
+    private static $instance;
+
+    public function __construct(){
+        session_start();
+    }
+
+    public static function getInstance(){
+        if (!self::$instance){
+            self::$instance = new Session();
+        }
+        return self::$instance;
+    }
+
+    public function set($key,$value){
+        //$_SESSION['key']= value;
+        $_SESSION[$key] = $value;
+    }
+
+
+    public function get($key){
+       if(isset($_SESSION[$key])){
+        return $_SESSION[$key];
+       }
+       return false;
+    }
+
+    public function unset_key($key){
+        unset($_SESSION[$key]);
+    }
+
+    public static function has($key){
+        return self::get($key);
+    }
+
+    public static function flash($key,$value='') {
+        if(empty($value)) {
+            $value = self::get($key);
+            self::unset_key($key);
+            return $value;
+        }
+        self::set($key,$value);
+    }
+
+    public function destroy(){
+        session_destroy();
+    }
+
+
+}
