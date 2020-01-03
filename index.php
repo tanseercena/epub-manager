a13f025e23f14da3e9fa89ae6d5f03e672e0ae28
humaismail-95 <humaismail19@gmail.com>
2020-01-03 16:49:02 +0600

    fixed merge

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<?php 
-require_once "config/init.php";
-// $user = new User();
-// echo $user->get_numeric('3'); // int(3)
-// echo $user->get_numeric('A'); // float(1.2)
-// echo $user->get_numeric('..');
-
-
-$user_id = Session::get("user_id");
-echo "User ID -> ".$user_id;
-
-?>
-=======
-=======
->>>>>>> upstream/master
 <?php
 // error_reporting(E_ALL);
 // ini_set('display_errors', 'On');
@@ -25,7 +8,3 @@ if(isset($user_id)){
 }
 
 ?>
-<<<<<<< HEAD
->>>>>>> upstream/master
-=======
->>>>>>> upstream/master
